4a906fca938224c74a34adb5dc82b15d1398f60f
dusainbolt <dule9xxpro@gmail.com>
2020-07-05 12:21:01 +0700

    team course part 1

--- src/pages/Web/Home/LandingPage/CourseTeam.jsx
+++ src/pages/Web/Home/LandingPage/CourseTeam.jsx
@@ -15,62 +15,91 @@ import {
   InstagramFilled,
   FacebookFilled,
   ContainerFilled,
-  EyeFilled
+  EyeFilled,
 } from "@ant-design/icons";
 
 const { Title, Paragraph } = Typography;
 
 class TopContent extends Component {
-  genderContentLeft = () => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        name: "Du Sainbolt",
+        url_avatar:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRH0jj8tNxcDI8aKOESEgrPGna-RMgUvNCs8w&usqp=CAU",
+      },
+    };
+  }
+
+  renderContentCourse = () => {
     return (
-      <div className="title">
-                 <Col className="container-ld--rows__col-course" span={12}>
-            {this.genderContentRight()}
+      <div>
+        <div className="rp-web">
+          <Col className="container-ld--rows__col-course" span={12}>
+            {this.genderContentLeft()}
           </Col>
           <Col className="container-ld--rows__col-course" span={12}>
-            {this.genderContentRight()}
+            {this.genderContentRight([32, 5])}
           </Col>
+        </div>
+        <div className="rp-mobie">
+          <Col className="container-ld--rows__col-course" span={24}>
+            {this.genderContentLeft()}
+          </Col>
+          <Col className="container-ld--rows__col-course" span={24}>
+            {this.genderContentRight([8, 5])}
+          </Col>
+        </div>
       </div>
     );
   };
 
-  genderContentRight = () => {
-    return <img className="img-background--top" src={ImgTop} alt="img top" />;
+  genderContentLeft = () => {
+    return (
+      <div className="container-ld--rows__col-course--content-left">
+        <div>sdsd</div>
+      </div>
+    );
+  };
+  genderContentRight = (objectGuitar = []) => {
+    return (
+      <div className="container-ld--rows__col-course--content-right">
+        <Row gutter={objectGuitar}>
+          <Col span={12}>
+            <div className="container-ld--rows__col-course--content-right__wrapper">
+              <div className="container-ld--rows__col-course--content-right__avatar">
+                <img src={this.state.user.url_avatar} alt="avatar" />
+              </div>
+              <div className="container-ld--rows__col-course--content-right__name">
+                {this.state.user.name}
+              </div>
+              <div className="container-ld--rows__col-course--content-right__icon-social">
+                <FacebookFilled className="container-ld--rows__col-course--content-right__icon-social--item"/>
+                <InstagramFilled className="container-ld--rows__col-course--content-right__icon-social--item"/>
+                <MailFilled className="container-ld--rows__col-course--content-right__icon-social--item"/>
+              </div>
+            </div>
+          </Col>
+          <Col span={12}>
+            <div className="container-ld--rows__col-course--content-right__wrapper">
+              sdsdsd
+            </div>
+          </Col>
+        </Row>
+      </div>
+    );
   };
 
   render() {
     const { t } = this.props;
     return (
-      <div>
+      <div className="container-ld--rows__course">
         <Carousel effect="fade">
-                     <Col span={24}>
-            {this.genderContentLeft()}
-          </Col>
-          <Col span={24}>
-            {this.genderContentLeft()}
-          </Col>
-          <Col span={24}>
-            {this.genderContentLeft()}
-          </Col>
-          {/* <Col className="rp-web" span={12}>
-            {this.genderContentLeft()}
-          </Col>
-          <Col className="rp-web" span={12}>
-            {this.genderContentLeft()}
-          </Col>
-          <Col className="rp-web" span={12}>
-            {this.genderContentLeft()}
-          </Col> */}
-          {/* <Col className="rp-web" span={12}>
-            {this.genderContentRight()}
-          </Col> */}
-          {/* <Col className="rp-mobie" span={24}>
-            {this.genderContentRight()}
-          </Col>
-          <Col className="rp-mobie" span={24}>
-            {this.genderContentLeft()}
-          </Col> */}
-          </Carousel>
+          <Col span={24}>{this.renderContentCourse()}</Col>
+          <Col span={24}>{this.renderContentCourse()}</Col>
+          <Col span={24}>{this.renderContentCourse()}</Col>
+        </Carousel>
       </div>
     );
   }
