3b1db81f031fc27d455ee685c32ff20c4a420c80
dusainbolt <dule9xxpro@gmail.com>
2020-07-13 23:08:12 +0700

    Updates

---
+++ precache-manifest.91c1fc73e49e65db758168589f2d857f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6be7c87b4ece278b2e9e3a682c12cb",
+    "url": "/app-du-frontend/index.html"
+  },
+  {
+    "revision": "cadf8dcb235d92603f62",
+    "url": "/app-du-frontend/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "612c4806c58ab7d39642",
+    "url": "/app-du-frontend/static/css/main.f126db86.chunk.css"
+  },
+  {
+    "revision": "cadf8dcb235d92603f62",
+    "url": "/app-du-frontend/static/js/2.2f62aff3.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/app-du-frontend/static/js/2.2f62aff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "612c4806c58ab7d39642",
+    "url": "/app-du-frontend/static/js/main.3ac0595e.chunk.js"
+  },
+  {
+    "revision": "eb5e06ff78230c73263f",
+    "url": "/app-du-frontend/static/js/runtime-main.bfaf7d3f.js"
+  },
+  {
+    "revision": "f3e7311344f4bf94b29ccd5d6826ab04",
+    "url": "/app-du-frontend/static/media/LoadingProject.f3e73113.gif"
+  },
+  {
+    "revision": "53b25a646f0bb655fc88b612f5c79b18",
+    "url": "/app-du-frontend/static/media/LogoSidebar.53b25a64.png"
+  },
+  {
+    "revision": "cbe050ca3532a3340a3ad373572c3fc9",
+    "url": "/app-du-frontend/static/media/bg-loginIT.cbe050ca.jpg"
+  },
+  {
+    "revision": "caf73798fb6f42cc5325b281459e0a54",
+    "url": "/app-du-frontend/static/media/error-page.caf73798.png"
+  },
+  {
+    "revision": "146ee029dc25ae4a428f12cce2d75054",
+    "url": "/app-du-frontend/static/media/logo.146ee029.png"
+  },
+  {
+    "revision": "b2643747439fd1b1b451120e9f767df8",
+    "url": "/app-du-frontend/static/media/top-img.b2643747.png"
+  }
+]);
