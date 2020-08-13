7d0b5100e5294704a8bcd6c2b5e6a02ad6a35a00
dusainbolt <dule9xxpro@gmail.com>
2020-08-13 10:34:29 +0700

    fox stre[

--- src/pages/Web/Home/LandingPage/DevelopmentProcessTest.jsx
+++ src/pages/Web/Home/LandingPage/DevelopmentProcessTest.jsx
@@ -1,42 +1,66 @@
 import React from "react";
-import { Steps, Button, message, Row } from "antd";
+import { Steps, Button, message, Row, Col } from "antd";
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 const { Step } = Steps;
 
-const steps = [
-  {
-    title: "First",
-    content: "First-content",
-  },
-  {
-    title: "Second",
-    content: "Second-content",
-  },
-  {
-    title: "Last",
-    content: "Last-content",
-  },
-];
-
 function DevelopmentProcess() {
   const [current, setCurrent] = useState(0);
-
+  const { t } = useTranslation();
   const next = () => () => {
     setCurrent(current + 1);
   };
 
   const prev = () => () => {
-    setCurrent(current + -1);
+    setCurrent(current - 1);
   };
+
+  const steps = [
+    {
+      title: t("ld_process.title_1"),
+      content: () => renderStepOne(),
+    },
+    {
+      title: t("ld_process.title_2"),
+      content: () => renderStepOne(),
+    },
+    {
+      title: t("ld_process.title_3"),
+      content: () => renderStepOne(),
+    },
+    {
+      title: t("ld_process.title_4"),
+      content: () => renderStepOne(),
+    },
+    {
+      title: t("ld_process.title_5"),
+      content: () => renderStepOne(),
+    },
+  ];
+
+  const renderStepOne = () => {
+    return (
+      <Row>
+        <Col xs={{ span: 24 }} sm={{ span: 12 }}>
+          Image
+        </Col>
+        <Col xs={{ span: 24 }} sm={{ span: 12 }}>
+          Text
+        </Col>
+      </Row>
+    );
+  };
+
   return (
     <Row className="ld-step">
+      <div className="container-ld--title">{t("development_process.process_title")}</div>
       <Steps current={current}>
         {steps.map(item => (
           <Step key={item.title} title={item.title} />
         ))}
       </Steps>
-      <div className="steps-content">{steps[current].content}</div>
+      <div className="steps-content">{steps[current].content()}</div>
       <div className="steps-action">
         {current < steps.length - 1 && (
           <Button type="primary" onClick={next()}>
