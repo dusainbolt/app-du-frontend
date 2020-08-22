b4673b6703ba277b4ad9eadcbcf211cd6f44503a
dusainbolt <dule9xxpro@gmail.com>
2020-08-23 00:03:54 +0700

    fix

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,10 +1,12 @@
 import { all } from "redux-saga/effects";
 
 import { watchLogin } from "../pages/Login/saga";
+import { watchSendEmail } from "../pages/Admin/Home/saga";
 
 function* rootSaga() {
   yield all([
     watchLogin(),
+    watchSendEmail(),
   ]);
 }
 export default rootSaga;
