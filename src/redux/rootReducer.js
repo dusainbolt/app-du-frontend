d768852072cc9e00711958eb54b0730a1a2af2b1
dusainbolt <dule9xxpro@gmail.com>
2020-09-02 10:16:26 +0700

    fix base

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,13 +1,9 @@
 import { combineReducers } from "redux";
 
-import demoReducer from "../pages/Admin/Home/reducer";
 import loginReducer from "../pages/Login/reducer";
 import layoutReducer from "../pages/Layout/reducer";
-import chatReducer from "../pages/Admin/Chat/reducer";
 
 export default combineReducers({
-  demoReducer,
   loginReducer,
   layoutReducer,
-  chatReducer,
 });
--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,14 +1,10 @@
 import { all } from "redux-saga/effects";
 
 import { watchLogin } from "../pages/Login/saga";
-import { watchSendMessage } from "../pages/Admin/Chat/saga";
-import { watchSendEmail } from "../pages/Admin/Home/saga";
 
 function* rootSaga() {
   yield all([
     watchLogin(),
-    watchSendEmail(),
-    watchSendMessage(),
   ]);
 }
 export default rootSaga;
